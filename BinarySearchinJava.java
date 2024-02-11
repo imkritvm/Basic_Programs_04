48c118b1b93d59349b42bbd2a6b4ef7202323703
Krit Vardhan Mishra <152051110+imkritvm@users.noreply.github.com>
2024-02-11 13:11:13 +0530

    BinarySearchinJava.java

--- BinarySearchinJava.java
+++ BinarySearchinJava.java
@@ -1,3 +1,4 @@
+// Binary Search program but for Sorted Array
 import java.util.Scanner;
 public class BinarySearchinJava
 {
@@ -22,6 +23,11 @@ public class BinarySearchinJava
     {
         Scanner sc = new Scanner(System.in);
         int[] num = {3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 61};
+        System.out.println("Given Array is : ");
+        for (int element: num) {
+            System.out.print(element+" ");
+        }
+        System.out.println("Enter the number which you want to search : ");
         int target = sc.nextInt();
         int result = binarySearch(num, 0, num.length - 1,target);
         if (result != -1)
