a83d4a38bf7761d4f0e778e468b9c13a4db974d8
Krit Vardhan Mishra <152051110+imkritvm@users.noreply.github.com>
2024-02-11 13:04:19 +0530

    Add files via upload
    Binary Search program but just for sorted array

---
+++ BinarySearchinJava.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class BinarySearchinJava
+{
+    public static int binarySearch(int[] ary, int start, int end, int target)
+    {
+        if (end >= start)
+        {
+            int mid = start + (end - start) / 2;
+            if (ary[mid] == target)
+            {
+                return mid;
+            }
+            if (ary[mid] < target)
+            {
+                return binarySearch(ary, mid + 1, end, target);
+            }
+            return binarySearch(ary, start, mid - 1, target);
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int[] num = {3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 61};
+        int target = sc.nextInt();
+        int result = binarySearch(num, 0, num.length - 1,target);
+        if (result != -1)
+        {
+            System.out.println("Index of "+target+" : " + result);
+        } else
+        {
+            System.out.println(target+" not found in the array.");
+        }
+    }
+}
