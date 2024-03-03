c5e4ee104d0d5a589305aebeb449b7f77f25469f
Krit Vardhan Mishra <152051110+imkritvm@users.noreply.github.com>
2024-03-04 00:31:04 +0530

    Update BigInteger.java

--- BigInteger.java
+++ BigInteger.java
@@ -1,16 +1,20 @@
-import java.io.*;
+import java.util.Scanner;
 import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        BigInteger a = new BigInteger(A);
+        BigInteger b = new BigInteger(B);
+        
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+        
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         String n = bufferedReader.readLine();
         boolean result;
         bufferedReader.close();
