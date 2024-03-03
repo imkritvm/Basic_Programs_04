0c0efb381827b4c0db771e0f267099b5eaffc2a1
Krit Vardhan Mishra <152051110+imkritvm@users.noreply.github.com>
2024-03-03 12:14:11 +0530

    Create BigInteger.java

---
+++ BigInteger.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        boolean result;
+        bufferedReader.close();
+        BigInteger bn = new BigInteger(n);
+        result = bn.isProbablePrime(1);
+        if(result==true){
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
